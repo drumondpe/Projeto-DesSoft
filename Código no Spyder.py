192aa0fd18b9b418abeecb6fbdf615ce5e794913
keiyanishio <keiyan@al.insper.edu.br>
2020-05-15 18:25:53 -0300

    Consertos

--- Código no Spyder.py
+++ Código no Spyder.py
@@ -4,20 +4,62 @@ Spyder Editor
 
 Este é um arquivo de script temporário.
 """
-
 import pygame
+import random
 
 pygame.init()
+pygame.mixer.init()
 
-##### CONFIGURAÇÕES BÁSICAS:
-#tela
+
+############ CONFIGURAÇÕES ##########
 LARGURA = 600
-ALTURA = 700
+ALTURA = 700 #config da tela
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption('Joguinho do come-come')
+FPS = 60
 
-#fontes
-tamanho_texto_tela.inicial = 20
-fonte_texto_tela.inicial = 'Futura XBlk BT'
+preto = (0, 0, 0) #config de cores
 
-##### Classe
-class Jogo:
+#config de fontes
+tamanho_texto_start = 18
+
+fonte_texto_start = 'Futura XBlk BT'
     
+    
+############ CLASSE ###############
+
+class Jogo:
+    def __init__(self):
+        self.tela = pygame.display.set_mode(LARGURA, ALTURA)
+        pygame.display.set_caption('Joguinho do come-come')
+        self.rodando = True
+
+    def rodar(self):
+        while self.rodando:
+            if self.estado == 'tela de inicio'
+            
+
+                
+########## Outras funções ############
+
+
+
+
+###### Funções da primeira tela ######
+    def comeco_eventos(self):
+        for evento in pygame.event.get():
+            if evento.digitado == pygame.QUIT:
+                self.rodando = False
+
+
+
+###### Funções da tela de jogo #######
+
+
+        
+        
+        
+        
+        
+        
+        
