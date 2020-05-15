5c0cc66772f265ad44beae8d4dc9cfe1e9938584
Pedro Drumond <pedrogsd@al.insper.edu.br>
2020-05-15 18:18:27 -0300

    Começando a classe

--- Código no Spyder.py
+++ Código no Spyder.py
@@ -9,8 +9,15 @@ import pygame
 
 pygame.init()
 
-#####CONFIGURAÇÕES BÁSICAS:
+##### CONFIGURAÇÕES BÁSICAS:
 #tela
 LARGURA = 600
 ALTURA = 700
 
+#fontes
+tamanho_texto_tela.inicial = 20
+fonte_texto_tela.inicial = 'Futura XBlk BT'
+
+##### Classe
+class Jogo:
+    
