79612680dcc8e1d4236c1d2602212326e673b25f
Pedro Drumond <pedrogsd@al.insper.edu.br>
2020-05-18 10:10:26 -0300

    Foto dos fantasmas

--- Jogo_principal.py
+++ Jogo_principal.py
@@ -4,3 +4,35 @@ Created on Fri May 15 19:26:29 2020
 @author: Pedro Drumond
 """
 
+import pygame
+pygame.init()
+
+
+#config da tela
+LARGURA = 500
+ALTURA = 600
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption('Joguinho do Come-Come')
+mapa = pygame.image.load('map-pacman.png').convert_alpha()
+
+tela.fill(0, 0, 0)
+tela.blit(mapa)
+
+####################### CONFIG DAS IMAGENS ####################################
+
+
+
+#configurações dos mobs - imagens (https://www.pngwing.com/pt/free-png-nyddf)
+LARGURA_FANTASMA = 30
+ALTURA_FANTASMA = 18
+
+#imagens dos mobs
+fantasma_verde_img = pygame.image.load('fantasma-verde.png').convert_alpha()
+fantasma_rosa_img = pygame.image.load('fantasma-rosa.png').convert_alpha()
+fantasma_vermelho_img = pygame.image.load('fantasma-vermelho.png').convert_alpha()
+fantasma_tiltado_img = pygame.image.load('fantasma-tiltado.png').convert_alpha()
+
+fantasma_verde = pygame.transform.scale(fantasma_verde_img, (LARGURA_FANTASMA, ALTURA_FANTASMA))
+fantasma_rosa = pygame.transform.scale(fantasma_rosa_img, (LARGURA_FANTASMA, ALTURA_FANTASMA))
+fantasma_vermelho = pygame.transform.scale(fantasma_vermelho_img, (LARGURA_FANTASMA, ALTURA_FANTASMA))
+fantasma_tiltado = pygame.transform.scale(fantasma_tiltado_img, (LARGURA_FANTASMA, ALTURA_FANTASMA))
