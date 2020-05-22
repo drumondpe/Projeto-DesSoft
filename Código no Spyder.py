8c49fc25d47204d4cb46ef31596fc576b5d8318b
keiyanishio <keiyan@al.insper.edu.br>
2020-05-22 18:40:38 -0300

    codigo spyder

--- Código no Spyder.py
+++ Código no Spyder.py
@@ -27,62 +27,44 @@ jogo = True
 
 #config de cores
 preto = (0, 0, 0) 
+branco = (255, 255, 255)
+rosa = (255, 0, 255)
+vermelho = (255, 0, 0)
+verde = (0, 255, 0)
+azul = (0, 0, 255)
+amarelo = (255, 255, 0)
 
 
-#config de fontes
-fonte_texto_inicial = pygame.font.SysFont('Futura ZBlk BT', 50)
+#config de fontes 
+fonte_texto_inicial = pygame.font.SysFont('Futura ZBlk BT', 50) 
 fonte_texto_nomes = pygame.font.SysFont('Futura ZBlk BT', 30)
+
+#textos que aparecem na primeira tela
 titulo_do_jogo = fonte_texto_inicial.render('Jogo do Come-Come', True, (200, 205, 70))
-botao_de_inicio1 = fonte_texto_inicial.render('Pressione Barra de Espaço', True, (255, 40, 255))
-botao_de_inicio2 = fonte_texto_inicial.render('para começar', True, (255, 40, 255))
+botao_de_inicio1 = fonte_texto_inicial.render('"Pressione Barra de Espaço', True, (255, 40, 255))
+botao_de_inicio2 = fonte_texto_inicial.render('para começar"', True, (255, 40, 255))
 nome_dos_criadores1 = fonte_texto_nomes.render('Keiya Nishio', True, (200, 90, 210))
 nome_dos_criadores2 = fonte_texto_nomes.render('Jerônimo Afrange', True, (200, 90, 210))
 nome_dos_criadores3 = fonte_texto_nomes.render('Pedro Drumond', True, (200, 90, 210))
 
+#posicionamento dos textos que aparecem na primeira tela
+tela.fill(preto)
+tela.blit(titulo_do_jogo, (90, 90))
+tela.blit(botao_de_inicio1, (25, 270))
+tela.blit(botao_de_inicio2, (135, 300))
+tela.blit(nome_dos_criadores1, (180, 490))
+tela.blit(nome_dos_criadores2, (180, 520))
+tela.blit(nome_dos_criadores3, (180, 550))
 
 #fundo = pygame.image.load('assests/img/map-pacman.png').convert()
 
+#decide se o jogo fecha ou passa para a próxima tela
 while jogo:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jogo = False
-    
-    tela.fill(preto)
-    tela.blit(titulo_do_jogo, (90, 200))
-    tela.blit(botao_de_inicio1, (30, 270))
-    tela.blit(botao_de_inicio2, (140, 300))
-    tela.blit(nome_dos_criadores1, (180, 490))
-    tela.blit(nome_dos_criadores2, (180, 520))
-    tela.blit(nome_dos_criadores3, (180, 550))
-
+        #if event.type == SPACE
 
 
     pygame.display.update()
 pygame.quit()
-
-    
-    
-############ CLASSE ###############
-
-
-            
-
-                
-########## Outras funções ############
-
-
-
-
-###### Funções da primeira tela ######
-   
-
-
-###### Funções da tela de jogo #######
-
-        
-        
-        
-        
-        
-        
-        
